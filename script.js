3ae51faec6271c64601e87cf74b831589fbfa1de
AJ202302271 <202302271@student.polytechnic.bh>
2025-12-10 22:04:58 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("shopBtn").addEventListener("click", function() {
   document.getElementById("products").scrollIntoView({ behavior: "smooth" });
 });
 
-document.getElementById("Scrub1")[1].onclick = function () { alert("Available!"); };
+document.getElementById("Scrub1").onclick = function () { alert("Available!"); };
 
 document.getElementById("regForm").addEventListener("submit", function(e) {
 e.preventDefault();
